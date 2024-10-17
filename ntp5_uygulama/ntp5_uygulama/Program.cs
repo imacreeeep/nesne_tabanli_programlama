03be2f95d3b4832ae559338172600aa91fe32b51
b <fltw11ck@gmail.com>
2024-10-17 14:52:17 +0300

    Add files via upload

---
+++ ntp5_uygulama/ntp5_uygulama/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ntp5_uygulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] ogrenciler = { "ali", "ayşe", "mehmet" };
+            //string[] dersler = {"matematik", "fizik", "kimya"};
+            //int[,] notlar = new int[3, 3]
+            //{
+            //    {85, 88, 75},
+            //    {90, 92, 85},
+            //    {78, 80, 90}
+            //};
+
+            //Console.WriteLine("öğrencilerin notları tablosu:");
+            //Console.WriteLine($"{"dersler", -12}");
+
+            //foreach(var ogrenci in ogrenciler)
+            //{
+            //    Console.Write($"{ogrenci, -10}");
+            //}
+            //Console.WriteLine();
+
+            //for (int i = 0; i < dersler.Length; i++)
+            //{
+            //    Console.Write($"{dersler[i], -12}");
+            //    for (int j = 0; j < ogrenciler.Length; j++)
+            //    {
+            //        Console.Write($"{notlar[j,i], -10}");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine();
+
+            //for(int i = 0;i < ogrenciler.Length; i++)
+            //{
+            //    int toplamnot = 0;
+            //    for (int j = 0;j < dersler.Length; j++)
+            //    {
+            //        toplamnot += notlar[i, j];
+            //    }
+            //    double ortalama = toplamnot / (double)dersler.Length;
+            //    Console.WriteLine($"{ogrenciler[i]} isimli öğrencinin ortalam notu: {ortalama:F2}");
+            //}
+
+
+            //string sifre = "123";
+            //string girilensifre;
+            //int hak = 3;
+            //int bakiye = 1000000;
+            //do
+            //{
+            //    Console.WriteLine("3 haneli şifrenizi giriniz");
+            //    girilensifre = Console.ReadLine();
+            //    if (girilensifre == sifre)
+            //    {
+            //        Console.WriteLine("bakiye sorgulamak için '1', çıkış yapmak için '2' yazın");
+            //        string islem = Console.ReadLine();
+            //        if (islem == "1") Console.WriteLine($"bakiyeniz: {bakiye}TL");
+            //        else if (islem == "2")
+            //        {
+            //            Console.WriteLine("çıkış yapılıyor");
+            //            break;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        hak--;
+            //        Console.WriteLine("hatalı giriş! kalan hakkınız: " + hak);
+            //        if (hak == 0)
+            //        {
+            //            Console.WriteLine("hesap kilitlendi");
+            //            break;
+            //        }
+            //    }
+            //} while (sifre != girilensifre);
+        }
+    }
+}
